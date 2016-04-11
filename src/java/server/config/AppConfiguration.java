da6f6320cd1534669e078c575bd2e8c85b56cf17
shanthala <shanthala@users.noreply.github.com>
2016-04-11 00:48:40 -0500

    Updated with implementation changes

--- src/java/server/config/AppConfiguration.java
+++ src/java/server/config/AppConfiguration.java
@@ -1,6 +1,11 @@
 package server.config;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.dropwizard.Configuration;
+import io.dropwizard.db.DataSourceFactory;
+
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 
 
 /**
@@ -8,4 +13,19 @@ import io.dropwizard.Configuration;
  */
 public class AppConfiguration extends Configuration
 {
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory dataSourceFactory) {
+        this.database = dataSourceFactory;
+    }
+
+
 }
